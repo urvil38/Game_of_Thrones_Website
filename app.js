a913c4841fe1a2aad28b01f3871b1297f8442e75
Urvil <patelurvil38@gmail.com>
2017-04-29 19:50:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var routes = require('./routes');
+var path = require('path');
+
+
+var app = express();
+app.set('view engine' , 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+var port = process.env.PORT || 8000;
+
+//app.get('/',routes.home);
+
+app.get('/seasons/:season_number/episodes/:episode_number',routes.episodes);
+
+app.get('/seasons/:season_number/episodes/',routes.episodeHome);
+
+app.listen(port,function(){
+    console.log(`app running on port : ${port}`);
+})
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var seriesDataJSON = require('../seasons.json');
+
+var seasons = seriesDataJSON.seasons;
+
+exports.episodes= function(req,res) {
+
+    var season_number = req.params.season_number;
+
+    var episode_number = req.params.episode_number;
+    
+    //element of particular season which index is calulated by help of season_number given by req.params property
+    var season = seasons[season_number - 1];
+    
+    //array of episodes of particular season i.e season 1 ,episode all
+    var episodes = season.episodes;
+
+    //element of particular episode which index is calculated by help of episode_number given by req.params property
+    var episode = season.episodes[episode_number - 1];
+
+    //reander episode based on particular information
+    res.render('episode',{
+        season : season,
+        season_number :season_number,
+        episodes : episodes,
+        episode : episode
+    });
+}
+
+exports.episodeHome = function(req,res) {
+
+    var season_number = req.params.season_number;
+
+    var season = seasons[season_number - 1];
+
+    var episodes = season.episodes;
+
+    res.render('episodeHome',{
+        season : season,
+        episodes : episodes,
+        season_number : season_number
+    });
+
+}
+
