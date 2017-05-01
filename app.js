7d4470d56a0ce2ecbdb23fbe6cf6e2745eae6ec0
Urvil <patelurvil38@gmail.com>
2017-05-01 15:22:23 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var routes = require('./routes/routes');
 var path = require('path');
+var favicon = require('serve-favicon');
 
 
 var app = express();
 app.set('view engine' , 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
-var port = process.env.PORT ;
+var port = process.env.PORT || 3000;
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.get('/',routes.seasonHome);
 
